dc62f5ba293b11233bfe35812b388d228b7552a7
Kanish-Kumar-S <kanishkumar.s2023@vitstudent.ac.in>
2026-01-18 11:56:49 +0530

    new-login-1

--- login.py
+++ login.py
@@ -1,5 +1,5 @@
+from datetime import date
 a=input("username:")
-
 db={"kanish kumar":"2005","tharun":"2005","vinay":"2003","charan":"333","velluri":"234"}
 while(a not in db.keys()):
     print("invalid  user")
@@ -8,5 +8,8 @@ b=input("password:")
 while not(db[a]==b):
     print("wrong password")
     b=input("password:")
-print("welcome",end=" "+a+"\n")
+
+print("welcome ",end=" "+a+"\n")
+print("date:")
+print(date.today())
 
