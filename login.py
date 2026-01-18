52a9bd81e60ac576c43ef32a47b50226e77cbcd7
Kanish-Kumar-S <kanishkumar.s2023@vitstudent.ac.in>
2026-01-18 11:14:30 +0530

    second-commit

---
+++ login.py
@@ -0,0 +1,10 @@
+a=input("username:")
+db={"kanish kumar":"2005","tharun":"2005","vinay":"2003","charan":"333","velluri":"234"}
+while(a not in db.keys()):
+    print("invalid  user")
+    a=input("username:")
+b=input("password:")
+while not(db[a]==b):
+    print("wrong password")
+    b=input("password:")
+print("welcome",end=" "+a+"\n")
